b3967c0e4e777c285f4df41e27c7d325018d07b6
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-10-15 22:53:57 +0500

    Add the Api integration

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,26 @@
-import jobs from "./../jobs.json"
+
+import { useEffect, useState } from "react"
 import JobListing from "./JobListing"
 
 const JobListings = ({ isHome = false }) => {
-    const allJobListing = isHome ? jobs.slice(0, 3) : jobs
-    console.log(allJobListing)
+    const [jobs, setJobs] = useState([])//it fill the response in the array
+    const [loading, setLoading] = useState(true)
+    console.log(jobs)
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const res = await fetch("http://localhost:8000/jobs")
+                const data = await res.json()
+                setJobs(data)
+            } catch (error) {
+                console.error("Jobs Data is not fetching right now...", error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchJobs()
+    }, [])
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -11,9 +28,12 @@ const JobListings = ({ isHome = false }) => {
                     {isHome ? "Recent Jobs" : "Browse Jobs"}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {allJobListing.map((job) => (
-                        <JobListing key={job.id} job={job} />
-                    ))}
+                    {loading ? (
+                        <h2>Loading</h2>
+                    ) : <>
+                        {jobs.map((job) => (
+                            <JobListing key={job.id} job={job} />
+                        ))}</>}
                 </div>
             </div>
         </section>
