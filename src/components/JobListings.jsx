ae92600e1cfe23cc4ad41105087dc81980aa12f4
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-10-26 14:55:04 +0500

    Add code

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,45 +1,46 @@
-
-import { useEffect, useState } from "react"
-import JobListing from "./JobListing"
-import Spinner from "./Spinner"
+import { useState, useEffect } from 'react';
+import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
-    const [jobs, setJobs] = useState([])//it fill the response in the array
-    const [loading, setLoading] = useState(true)
-    console.log(jobs)
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchJobs = async () => {
-            const apiURL = `/api/${isHome ? '?_limit=3' : ''}`;
+            const apiUrl = isHome ? '/api/?_limit=3' : '/api/';
             try {
-                const res = await fetch(apiURL)
-                const data = await res.json()
-                setJobs(data)
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJobs(data);
             } catch (error) {
-                console.error("Jobs Data is not fetching right now...", error);
+                console.log('Error fetching data', error);
             } finally {
-                setLoading(false)
+                setLoading(false);
             }
-        }
-        fetchJobs()
-    }, [])
+        };
+
+        fetchJobs();
+    }, []);
+
     return (
-        <section className="bg-blue-50 px-4 py-10">
-            <div className="container-xl lg:container m-auto">
-                <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    {isHome ? "Recent Jobs" : "Browse Jobs"}
+        <section className='bg-blue-50 px-4 py-10'>
+            <div className='container-xl lg:container m-auto'>
+                <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+                    {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
+
                 {loading ? (
                     <Spinner loading={loading} />
-                ) :
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                ) : (
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
                         {jobs.map((job) => (
                             <JobListing key={job.id} job={job} />
                         ))}
-                    </div>}
+                    </div>
+                )}
             </div>
         </section>
-    )
-}
-
-export default JobListings
+    );
+};
+export default JobListings;
